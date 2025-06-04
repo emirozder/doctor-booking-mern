99be3097b6d392c2ff7ceb2868e6ed480dff63b9
Emir Özder <emir.ozder@hotmail.com>
2025-06-04 22:07:27 +0300

    fix: footer margin top

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 const Footer = () => {
   return (
     <footer className="md:mx-10">
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-24 text-sm">
         {/* LEFT SECTION */}
         <div>
           <img
