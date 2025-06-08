304b7cc603bfc448819ddc15d5c33f2d688fbed5
Emir Özder <emir.ozder@hotmail.com>
2025-06-08 19:00:17 +0300

    feat: admin sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,13 @@
 import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import { AdminContext } from "./context/AdminContext";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import Dashboard from "./pages/Admin/Dashboard";
+import DoctorsList from "./pages/Admin/DoctorsList";
 import Login from "./pages/Login";
 
 const App = () => {
@@ -11,6 +17,16 @@ const App = () => {
     <div className="font-outfit">
       <ToastContainer />
       <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <div className="font-outfit">
