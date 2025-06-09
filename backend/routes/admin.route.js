6b44e82c050bb36d1bca8160bdf1b32e66d84d84
Emir Özder <emir.ozder@hotmail.com>
2025-06-10 00:05:01 +0300

    feat: change-availability endpoint

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { addDoctor, getAllDoctors, loginAdmin } from '../controllers/admin.controller.js';
+import { changeAvailability } from '../controllers/doctor.controller.js';
 import { authAdmin } from '../middllewares/authAdmin.middleware.js';
 import upload from '../middllewares/multer.js';
 
@@ -8,5 +9,6 @@ const adminRouter = express.Router();
 adminRouter.post('/login', loginAdmin)
 adminRouter.get('/get-all-doctors', authAdmin, getAllDoctors)
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
+adminRouter.post('/change-availability', authAdmin, changeAvailability)
 
 export default adminRouter;
