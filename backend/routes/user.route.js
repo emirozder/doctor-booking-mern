d36393670db959822fef46bf95bcd145710ad292
Emir Özder <emir.ozder@hotmail.com>
2025-06-11 01:35:40 +0300

    feat: user model and user register endpoint

---
+++ backend/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { userRegister } from '../controllers/user.controller.js';
+
+
+const userRouter = express.Router();
+
+userRouter.post('/register', userRegister)
+
+export default userRouter;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,13 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (email, password) => {
+export const generateAdminToken = (email, password) => {
   const token = jwt.sign({ email, password }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+  return token
+}
+
+export const generateUserToken = (userId) => {
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
   return token
 }
