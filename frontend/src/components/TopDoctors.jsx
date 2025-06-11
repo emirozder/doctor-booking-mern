45dfb0122dfc83ce05d682022cfee0180fe53148
Emir Özder <emir.ozder@hotmail.com>
2025-06-11 23:26:55 +0300

    feat: fetch doctors on TopDoctors component

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import DoctorCard from "./DoctorCard";
 
 const TopDoctors = () => {
   const navigate = useNavigate();
-  const { doctors } = useContext(AppContext);
+  const { doctors, fetchDoctors, doctorsLoading } = useContext(AppContext);
+
+  useEffect(() => {
+    fetchDoctors();
+  }, []);
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
@@ -13,11 +18,21 @@ const TopDoctors = () => {
       <p className="sm:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors.
       </p>
-      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((doctor) => (
-          <DoctorCard key={doctor._id} doctor={doctor} />
-        ))}
-      </div>
+      {doctorsLoading ? (
+        <div className="w-full h-96 flex items-center justify-center">
+          <img
+            src={assets.loading_icon}
+            alt="loading"
+            className="size-8 animate-spin"
+          />
+        </div>
+      ) : (
+        <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+          {doctors.slice(0, 10).map((doctor) => (
+            <DoctorCard key={doctor._id} doctor={doctor} />
+          ))}
+        </div>
+      )}
       <button
         onClick={() => {
           navigate("/doctors");
