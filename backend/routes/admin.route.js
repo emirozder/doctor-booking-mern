babb5e0c7bd335cd929484ac1cbe26386007eaf4
Emir Özder <emir.ozder@hotmail.com>
2025-06-17 17:10:24 +0300

    feat: /get-all-appointments endpoint for admin

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addDoctor, getAllDoctors, loginAdmin } from '../controllers/admin.controller.js';
+import { addDoctor, getAllAppointments, getAllDoctors, loginAdmin } from '../controllers/admin.controller.js';
 import { changeAvailability } from '../controllers/doctor.controller.js';
 import { authAdmin } from '../middllewares/authAdmin.middleware.js';
 import upload from '../middllewares/multer.js';
@@ -10,5 +10,6 @@ adminRouter.post('/login', loginAdmin)
 adminRouter.get('/get-all-doctors', authAdmin, getAllDoctors)
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
 adminRouter.post('/change-availability', authAdmin, changeAvailability)
+adminRouter.get('/get-all-appointments', authAdmin, getAllAppointments)
 
 export default adminRouter;
