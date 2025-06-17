42bab37d878f62835e2a130764d539c4a1258ef6
Emir Özder <emir.ozder@hotmail.com>
2025-06-17 23:46:17 +0300

    feat: /admin-dashboard endpoint for admin

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addDoctor, cancelAppointment, getAllAppointments, getAllDoctors, loginAdmin } from '../controllers/admin.controller.js';
+import { addDoctor, adminDashboard, cancelAppointment, getAllAppointments, getAllDoctors, loginAdmin } from '../controllers/admin.controller.js';
 import { changeAvailability } from '../controllers/doctor.controller.js';
 import { authAdmin } from '../middllewares/authAdmin.middleware.js';
 import upload from '../middllewares/multer.js';
@@ -12,5 +12,6 @@ adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
 adminRouter.post('/change-availability', authAdmin, changeAvailability)
 adminRouter.get('/get-all-appointments', authAdmin, getAllAppointments)
 adminRouter.post('/cancel-appointment', authAdmin, cancelAppointment)
+adminRouter.get('/admin-dashboard', authAdmin, adminDashboard)
 
 export default adminRouter;
